0565268b28320fbb1a75ee06d38c0404c0fe3126
Joe-Lindie <josephlindie@hotmail.co.uk>
2022-07-07 13:52:11 +0100

    started the check Items Off Fn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //GRABS ELEMENTS FROM DOM
-const userInput = document.getElementById("user-input");
-const submitBtn = document.getElementById("submit-btn");
-const newTodo = document.getElementById("todo");
+const userInput = document.getElementById("user-input")
+const submitBtn = document.getElementById("submit-btn")
+const newTodo = document.getElementById("todo")
 
 //EVENTLISTENER(S)
-submitBtn.addEventListener("click", addNewTodoItem);
+submitBtn.addEventListener("click", addNewTodoItem)
 
 /////////////////////////////////////////////////
 
@@ -13,7 +13,7 @@ submitBtn.addEventListener("click", addNewTodoItem);
 //////////////////////////////////////////////////
 
 function addNewTodoItem() {
-  const newTodoItem = userInput.value;
+  const newTodoItem = userInput.value
   // const newListElement = document.createElement("li");
   // newListElement.innerHTML = newTodoItem;
   // newListElement.classList.add("new_todo_item");
@@ -25,14 +25,45 @@ function addNewTodoItem() {
   // checkbox.setAttribute("checked", "");
   // checkbox.classList.add("toggle");
   // newListElement.appendChild(checkbox);
-  //
-  const todo = document.createElement("li");
-  todo.classList = "new_todo_item";
+
+  const todo = document.createElement("li")
+
+  // if (todo) {
+  //   todo.addEventListener("change", checkItemsOffList)
+  // }
+
+  todo.classList = "new_todo_item"
   const todoContent = `<div class="view">
   <input class="toggle" type="checkbox">
   <label>${newTodoItem}</label>
   <button class="destroy"></button>
-</div>`;
-  todo.innerHTML = todoContent;
-  newTodo.appendChild(todo);
+</div>`
+  todo.innerHTML = todoContent
+  newTodo.appendChild(todo)
+
+  const toggle = document.querySelector(".toggle")
+  toggle.addEventListener("change", checkItemsOffList)
+}
+
+/////////////////////////////////////////////////
+
+//CHECK ITEMS OFF TODO LIST FUNCTION
+
+//////////////////////////////////////////////////
+
+function checkItemsOffList(event) {
+  // for (let i = 0; i < todo.length; i++) {
+  //   console.log(todo[i])
+  // }
+
+  if (event.target.checked === true) {
+    console.log("hiiii")
+    let parentDiv = event.target.parentNode
+    parentDiv.classList.add("checked")
+  }
+
+  if (event.target.checked === false) {
+    console.log("Not checked")
+    todo.style.textDecoration = "none"
+  }
 }
