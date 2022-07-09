808040d7a2d7de8ff9e5f24540da3f9d5615c892
Joe-Lindie <josephlindie@hotmail.co.uk>
2022-07-09 13:54:54 +0100

    Clears user input after submit button clicked

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //GRABS ELEMENTS FROM DOM
-const userInput = document.getElementById("user-input");
-const submitBtn = document.getElementById("submit-btn");
-const newTodo = document.getElementById("todo");
+const userInput = document.getElementById("user-input")
+const submitBtn = document.getElementById("submit-btn")
+const newTodo = document.getElementById("todo")
 
 //EVENTLISTENER(S)
-submitBtn.addEventListener("click", addNewTodoItem);
+submitBtn.addEventListener("click", addNewTodoItem)
 
 /////////////////////////////////////////////////
 
@@ -13,39 +13,30 @@ submitBtn.addEventListener("click", addNewTodoItem);
 //////////////////////////////////////////////////
 
 function addNewTodoItem() {
-  const newTodoItem = userInput.value;
-  // const newListElement = document.createElement("li");
-  // newListElement.innerHTML = newTodoItem;
-  // newListElement.classList.add("new_todo_item");
-  // newTodo.appendChild(newListElement);
+  const newTodoItem = userInput.value
+  userInput.value = ""
 
-  // // Create checkbox
-  // const checkbox = document.createElement("input");
-  // checkbox.setAttribute("type", "checkbox");
-  // checkbox.setAttribute("checked", "");
-  // checkbox.classList.add("toggle");
-  // newListElement.appendChild(checkbox);
+  const todo = document.createElement("li")
 
-  const todo = document.createElement("li");
-
-  todo.classList = "new_todo_item";
+  todo.classList = "new_todo_item"
   const todoContent = `<div class="view">
   <input class="toggle" type="checkbox">
   <label>${newTodoItem}</label>
   <button class="destroy"></button>
-</div>`;
-  todo.innerHTML = todoContent;
-  newTodo.appendChild(todo);
+</div>`
+  todo.innerHTML = todoContent
+  newTodo.appendChild(todo)
 
-  // SELECT ALL CLASSES OF TOGGLE AND LOOP
-  const toggle = document.querySelectorAll(".toggle");
-  const destroy = document.querySelectorAll(".destroy");
+  // SELECT ALL CLASSES OF TOGGLE/DESTROY AND LOOP
+  const toggle = document.querySelectorAll(".toggle")
+  const destroy = document.querySelectorAll(".destroy")
 
   for (let i of toggle) {
-    i.addEventListener("change", checkItemsOffList);
+    i.addEventListener("change", checkItemsOffList)
   }
+
   for (let i of destroy) {
-    i.addEventListener("click", deleteItemsOffList);
+    i.addEventListener("click", deleteItemsOffList)
   }
 }
 
@@ -57,16 +48,16 @@ function addNewTodoItem() {
 
 function checkItemsOffList(event) {
   if (event.target.checked) {
-    let parentDiv = event.target.parentNode;
-    parentDiv.classList.add("checked");
+    let parentDiv = event.target.parentNode
+    parentDiv.classList.add("checked")
   }
 
   if (event.target.checked === false) {
-    let parentDiv = event.target.parentNode;
-    parentDiv.classList.remove("checked");
+    let parentDiv = event.target.parentNode
+    parentDiv.classList.remove("checked")
   }
 }
 function deleteItemsOffList(event) {
-  console.log(event.target.parentNode);
-  event.target.parentNode.parentNode.remove();
+  console.log(event.target.parentNode)
+  event.target.parentNode.parentNode.remove()
 }
