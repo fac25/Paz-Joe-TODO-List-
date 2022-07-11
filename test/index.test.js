5e6a8db74130b0f6a9feba8422d5502ff7f0b4c0
Joe-Lindie <josephlindie@hotmail.co.uk>
2022-07-11 12:16:54 +0100

    checkbox test function

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,7 @@
-test("todo item is being added correctly", () => {
-  // get all three inputs
-  const input = document.querySelector("input");
-  const list = document.querySelector("#todo");
-  //const bInput = document.querySelector("input[name='b']");
+test("Is the todo item being added correctly ?", () => {
+  // get inputs
+  const input = document.querySelector("input")
+  const list = document.querySelector("#todo")
 
   // enter test data into inputs
   const inputs = [
@@ -11,13 +10,39 @@ test("todo item is being added correctly", () => {
     "Call the plumber at 07783978122",
     "Go part@£$ª¶YY",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam interdum nisi augue, vitae convallis nisl aliquet ut. Suspendisse lacinia felis eget metus hendrerit mollis. Aenean tempus felis quis nibh viverra pulvinar. Quisque rutrum malesuada faucibus. Maecenas quis dictum quam. Sed ante magna, ultrices non mi vitae, convallis pellentesque magna. Maecenas venenatis auctor pellentesque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque luctus ultrices fermentum.",
-  ];
+  ]
+
   inputs.forEach((element) => {
-    input.value = element;
-    const submitButton = document.querySelector("button[type='submit']");
-    submitButton.click();
-    const result = document.querySelector("li:last-child");
-    equal(result.textContent, element);
-    result.textContent = "";
-  });
-});
+    input.value = element
+    const submitButton = document.querySelector("button[type='submit']")
+
+    //addNewTodoItem() function is being invoked
+    submitButton.click()
+
+    const result = document.querySelectorAll("label")
+    const lastchild = result[result.length - 1]
+
+    if (input.value.length > 0) {
+      equal(lastchild.textContent, element)
+    }
+  })
+  // list.textContent = "";
+})
+
+/////////////////////////////////////////////////
+
+//TEST FOR CHECKBOXES
+
+//////////////////////////////////////////////////
+
+test("Checking an entry marks it as complete", () => {
+  const checkboxes = document.querySelectorAll(".toggle")
+
+  checkboxes.forEach((ele) => {
+    ele.click()
+    const actual = ele.parentElement.classList.contains("checked")
+    const expected = true
+
+    equal(actual, expected)
+  })
+})
