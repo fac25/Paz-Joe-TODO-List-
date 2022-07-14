aad8a53f8ec252e2cd8e1c7d48b7ad9a479643c4
Maria Paz <mariapazgar@gmail.com>
2022-07-14 16:06:28 +0100

    tester func for AddNewTodo

--- test/index.test.js
+++ test/index.test.js
@@ -26,6 +26,24 @@
 //     }
 //   })
 // })
+/////////////////////////////////////////////////
+
+//TEST FOR ADDING TODO - addNewTodoItem()
+
+//////////////////////////////////////////////////
+test("Is the todo item being added correctly ?", () => {
+  const userInput = document.querySelector("#user-input");
+  const submitBtn = document.querySelector("#submit-btn");
+  const list = document.querySelector("#todo");
+  userInput.value = "Test entry";
+  submitBtn.click();
+  const result = document.querySelectorAll("li");
+  const actual = result.length;
+  const expected = 1;
+
+  equal(actual, expected);
+  list.innerHTML = "";
+});
 
 /////////////////////////////////////////////////
 
@@ -46,23 +64,23 @@
 // })
 
 test("Checking an entry marks it as complete", () => {
-  const userInput = document.querySelector("#user-input")
-  const submitBtn = document.querySelector("#submit-btn")
+  const userInput = document.querySelector("#user-input");
+  const submitBtn = document.querySelector("#submit-btn");
 
-  userInput.value = "Test entry"
-  submitBtn.click()
+  userInput.value = "Test entry";
+  submitBtn.click();
 
   // SIMULATE CHECKING CHECKBOX
-  const checkbox = document.getElementsByClassName("toggle")
-  checkbox[0].click()
+  const checkbox = document.getElementsByClassName("toggle");
+  checkbox[0].click();
 
-  const actual = checkbox[0].checked
-  const expected = true
+  const actual = checkbox[0].checked;
+  const expected = true;
 
-  equal(actual, expected)
+  equal(actual, expected);
 
-  document.querySelector("#todo").innerHTML = ""
-})
+  document.querySelector("#todo").innerHTML = "";
+});
 
 /////////////////////////////////////////////////
 
@@ -71,21 +89,21 @@ test("Checking an entry marks it as complete", () => {
 //////////////////////////////////////////////////
 
 test("Test for deleting single entry", () => {
-  const userInput = document.querySelector("#user-input")
-  const submitBtn = document.querySelector("#submit-btn")
+  const userInput = document.querySelector("#user-input");
+  const submitBtn = document.querySelector("#submit-btn");
 
-  userInput.value = "Test entry"
-  submitBtn.click()
+  userInput.value = "Test entry";
+  submitBtn.click();
 
-  const deleteBtn = document.querySelector(".destroy")
-  deleteBtn.click()
+  const deleteBtn = document.querySelector(".destroy");
+  deleteBtn.click();
 
-  const exists = document.querySelector("user-input")
+  const exists = document.querySelector("user-input");
 
-  equal(exists, null)
+  equal(exists, null);
 
-  document.querySelector("#todo").innerHTML = ""
-})
+  document.querySelector("#todo").innerHTML = "";
+});
 
 /////////////////////////////////////////////////
 
@@ -94,100 +112,100 @@ test("Test for deleting single entry", () => {
 //////////////////////////////////////////////////
 
 test("Test All button in filter function displays all todos", () => {
-  const userInput = document.querySelector("#user-input")
-  const submitBtn = document.querySelector("#submit-btn")
+  const userInput = document.querySelector("#user-input");
+  const submitBtn = document.querySelector("#submit-btn");
 
   const inputs = [
     "Test entry",
     "Don't forget the milk",
     "Call the plumber at 07783978122",
-  ]
+  ];
 
   inputs.forEach((input) => {
-    userInput.value = input
-    submitBtn.click()
-  })
+    userInput.value = input;
+    submitBtn.click();
+  });
 
   // SIMULATE CLICKING ALL BTN
-  const allBtn = document.querySelector("#all-btn")
-  allBtn.click()
+  const allBtn = document.querySelector("#all-btn");
+  allBtn.click();
 
-  const actual = inputs.length
-  const expected = 3
+  const actual = inputs.length;
+  const expected = 3;
 
-  equal(actual, expected)
+  equal(actual, expected);
 
   //Reset
-  document.querySelector("#todo").innerHTML = ""
-})
+  document.querySelector("#todo").innerHTML = "";
+});
 
 //////////////////////////////////////////////////
 
 test("Test Active button in filter fn displays ONLY active todos", () => {
   //Adds entries to todo list
-  const userInput = document.querySelector("#user-input")
-  const submitBtn = document.querySelector("#submit-btn")
+  const userInput = document.querySelector("#user-input");
+  const submitBtn = document.querySelector("#submit-btn");
 
   const inputs = [
     "Test entry",
     "Don't forget the milk",
     "Call the plumber at 07783978122",
-  ]
+  ];
 
   inputs.forEach((input) => {
-    userInput.value = input
-    submitBtn.click()
-  })
+    userInput.value = input;
+    submitBtn.click();
+  });
 
   //checks off one entry in todo list
-  const checkbox = document.getElementsByClassName("toggle")
-  checkbox[0].click()
+  const checkbox = document.getElementsByClassName("toggle");
+  checkbox[0].click();
 
   // SIMULATE CLICKING ACTIVE BTN
-  const activeBtn = document.querySelector("#active-btn")
-  activeBtn.click()
+  const activeBtn = document.querySelector("#active-btn");
+  activeBtn.click();
 
   // Because we checked off one box, the output should be length - 1
-  const actual = inputs.length - 1
-  const expected = 2
+  const actual = inputs.length - 1;
+  const expected = 2;
 
-  equal(actual, expected)
+  equal(actual, expected);
 
   //Reset
-  document.querySelector("#todo").innerHTML = ""
-})
+  document.querySelector("#todo").innerHTML = "";
+});
 
 //////////////////////////////////////////////////
 
 test("tests completed button in filter fn displays ONLY completed todos", () => {
   //Adds entries to todo list
-  const userInput = document.querySelector("#user-input")
-  const submitBtn = document.querySelector("#submit-btn")
+  const userInput = document.querySelector("#user-input");
+  const submitBtn = document.querySelector("#submit-btn");
 
   const inputs = [
     "Test entry",
     "Don't forget the milk",
     "Call the plumber at 07783978122",
-  ]
+  ];
 
   inputs.forEach((input) => {
-    userInput.value = input
-    submitBtn.click()
-  })
+    userInput.value = input;
+    submitBtn.click();
+  });
 
   //checks off one entry in todo list
-  const checkbox = document.getElementsByClassName("toggle")
-  checkbox[0].click()
+  const checkbox = document.getElementsByClassName("toggle");
+  checkbox[0].click();
 
   // SIMULATE CLICKING COMPLETED BTN
-  const completedBtn = document.querySelector("#completed-btn")
-  completedBtn.click()
+  const completedBtn = document.querySelector("#completed-btn");
+  completedBtn.click();
 
-  const actual = inputs.length - 2
-  const expected = 1
+  const actual = inputs.length - 2;
+  const expected = 1;
 
-  equal(actual, expected)
+  equal(actual, expected);
 
   //Reset
-  document.querySelector("#todo").innerHTML = ""
-})
+  document.querySelector("#todo").innerHTML = "";
+});
