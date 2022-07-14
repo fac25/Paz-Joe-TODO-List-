c2e82cb3e516880e19f613bed78f8be3d91a5908
Maria Paz <mariapazgar@gmail.com>
2022-07-14 16:51:59 +0100

    this is an improved version

--- test/index.test.js
+++ test/index.test.js
@@ -32,14 +32,29 @@
 
 //////////////////////////////////////////////////
 test("Is the todo item being added correctly ?", () => {
-  const userInput = document.querySelector("#user-input");
+  //const userInput = document.querySelector("#user-input");
   const submitBtn = document.querySelector("#submit-btn");
   const list = document.querySelector("#todo");
-  userInput.value = "Test entry";
-  submitBtn.click();
+  //userInput.value = "Test entry";
+  const input = document.querySelector("input");
+  // enter test data into inputs
+  const userInput = [
+    "",
+    "  ",
+    "Call the plumber at 07783978122",
+    "Go part@£$ª¶YY",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam interdum nisi augue, vitae convallis nisl aliquet ut. Suspendisse lacinia felis eget metus hendrerit mollis. Aenean tempus felis quis nibh viverra pulvinar. Quisque rutrum malesuada faucibus. Maecenas quis dictum quam. Sed ante magna, ultrices non mi vitae, convallis pellentesque magna. Maecenas venenatis auctor pellentesque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque luctus ultrices fermentum.",
+  ];
+  const trimedArr = userInput.filter((el) => el.trim() !== "");
+
+  trimedArr.forEach((el) => {
+    input.value = el;
+    //addNewTodoItem() function is being invoked
+    submitBtn.click();
+  });
   const result = document.querySelectorAll("li");
   const actual = result.length;
-  const expected = 1;
+  const expected = trimedArr.length;
 
   equal(actual, expected);
   list.innerHTML = "";
